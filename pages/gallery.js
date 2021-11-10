d8c171deafe1e665f9a4e3a71dba2f458e6441b0
thryec <thryec@gmail.com>
2021-11-10 19:30:54 +0800

    added listedItems state to keep track of minted tokenIds

---
+++ pages/gallery.js
@@ -0,0 +1,7 @@
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from 'web3modal'
+import { nftaddress, marketplaceaddress } from '../../config'
+import NFT from '../../hardhat/artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../../hardhat/artifacts/contracts/Marketplace.sol/Marketplace.json'
