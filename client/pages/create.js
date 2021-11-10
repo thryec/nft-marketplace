110df93af6a433f30aca046326cd91987d8c688f
thryec <thryec@gmail.com>
2021-11-10 17:39:02 +0800

    succesfully connects to metamask

--- client/pages/create.js
+++ client/pages/create.js
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import { ethers } from 'ethers'
 import { useRouter } from 'next/dist/client/router'
 import Web3Modal from 'web3modal'
-import { create as ipfsClient } from 'ipfs-http-client'
+import { create } from 'ipfs-http-client'
 import { nftaddress, nftmarketaddress } from '../../config'
 import NFT from '../../hardhat/artifacts/contracts/NFT.sol/NFT.json'
 import Market from '../../hardhat/artifacts/contracts/Marketplace.sol/Marketplace.json'
 
-const client = ipfsClient('https://ipfs.infura.io:5001/api/v0')
+const client = create('https://ipfs.infura.io:5001/api/v0')
 
 const Create = () => {
-  const [itemUrl, setItemUrl] = useState('')
+  const [fileUrl, setFileUrl] = useState('')
   const [itemInfo, setItemInfo] = useState({
     name: '',
     description: '',
@@ -18,44 +18,80 @@ const Create = () => {
     quantity: '',
   })
 
-  const uploadToIpfs = () => {}
+  const onFileUpload = async (e) => {
+    const file = e.target.files[0]
+    try {
+      const addedFile = await client.add(file)
+      const url = `https://ipfs.infura.io/ipfs/${addedFile.path}`
+      setFileUrl(url)
+    } catch (e) {
+      console.log('Error uploading file: ', e)
+    }
+  }
 
-  const listOnMarketplace = () => {}
+  const createSaleItem = async () => {
+    const { name, description, price, quantity } = itemInfo
+    if (!name || !description || !price || !quantity || !fileUrl) {
+      alert('Please do not leave any fields blank.')
+      return
+    }
+    const data = JSON.stringify({ name, description, image: fileUrl })
+    try {
+      const addedFile = await client.add(data)
+      const url = `https://ipfs.infura.io/ipfs/${addedFile.path}`
+      console.log('image successfully uploaded to IPFS at url: ', url)
+      listOnMarketplace(url)
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }
+  }
+
+  const listOnMarketplace = async (url) => {
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+    console.log('provider: ', provider)
+    // mint nft
+    // set uri as url
+    // list on marketplace
+  }
 
   return (
     <div style={bodyStyle}>
       <h2>Upload Your Creation Here</h2>
       <div style={inputStyle}>
         <input
-          onChange={(e) => setItemInfo({ ...itemInfo, name: e.target.value })}
           placeholder="Item Name"
+          onChange={(e) => setItemInfo({ ...itemInfo, name: e.target.value })}
           style={inputFields}
         />
         <textarea
-          onChange={(e) => setItemInfo({ ...itemInfo, description: e.target.value })}
           placeholder="Item Description"
+          onChange={(e) => setItemInfo({ ...itemInfo, description: e.target.value })}
           style={inputFields}
         />
         <input
-          onChange={(e) => setItemInfo({ ...itemInfo, price: e.target.value })}
           placeholder="List Price"
+          onChange={(e) => setItemInfo({ ...itemInfo, price: e.target.value })}
           style={inputFields}
         />
         <input
-          onChange={(e) => setItemInfo({ ...itemInfo, quantity: e.target.value })}
           placeholder="List Quantity"
+          onChange={(e) => setItemInfo({ ...itemInfo, quantity: e.target.value })}
           style={inputFields}
         />
-        <input type="file" style={inputFields} />
+        <input type="file" onChange={onFileUpload} style={inputFields} />
         <button
           onClick={() => {
-            uploadToIpfs()
+            createSaleItem()
             listOnMarketplace()
           }}
           style={inputFields}
         >
           List Item
         </button>
+        {fileUrl && <img src={fileUrl} width="800px" />}
       </div>
     </div>
   )
@@ -69,7 +105,8 @@ const inputStyle = {
 }
 const inputFields = {
   width: '750px',
-  marginTop: 20,
+  marginTop: 10,
+  marginBottom: 10,
 }
 
 export default Create
