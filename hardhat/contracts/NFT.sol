8b57f7697c4097a66b67ac6a78d5e30a028afc7f
thryec <thryec@gmail.com>
2021-11-13 17:24:59 +0800

    refactored token minting function to include approvals and setting URI

--- hardhat/contracts/NFT.sol
+++ hardhat/contracts/NFT.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "hardhat/console.sol";
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+import 'hardhat/console.sol';
 
 contract NFT is ERC1155, Ownable {
     using Counters for Counters.Counter;
@@ -14,36 +14,43 @@ contract NFT is ERC1155, Ownable {
     // to store unique URIs for each nft
     mapping(uint => string) private _uris;
 
-    constructor(address marketAddress) ERC1155("VastOcean") {
+    constructor(address marketAddress) ERC1155('VastOcean') {
         marketplaceAddress = marketAddress;
     }
 
     // ------------------ Mutative Functions ---------------------- //
 
     function mintToken(
-        address account,
-        uint amount,
+        string memory tokenURI,
+        uint quantity,
         bytes memory data
     ) public returns (uint _tokenId) {
-        uint tokenId = _tokenIds.current(); 
-        _mint(account, tokenId, amount, data);
-        _tokenIds.increment(); 
-        return _tokenId; 
+        _tokenIds.increment();
+        uint currentTokenId = _tokenIds.current();
+
+        _mint(msg.sender, currentTokenId, quantity, data);
+        console.log('done minting token');
+        setApprovalForAll(marketplaceAddress, true);
+        console.log('done setting approval');
+        setTokenURI(currentTokenId, tokenURI);
+        console.log('done setting URI');
+
+        return _tokenId;
     }
 
     function setTokenURI(uint _tokenId, string memory newURI) public {
         bool owner = checkIfOwner(msg.sender, _tokenId);
         require(owner == true);
-        require(bytes(_uris[_tokenId]).length == 0, "Cannot set URI twice.");
+        require(bytes(_uris[_tokenId]).length == 0, 'Cannot set URI twice.');
         _uris[_tokenId] = newURI;
     }
 
     function burnTokens(
         address from,
         uint _tokenId,
-        uint amount
+        uint quantity
     ) public {
-        _burn(from, _tokenId, amount);
+        _burn(from, _tokenId, quantity);
     }
 
     // ------------------ Read Functions ---------------------- //
