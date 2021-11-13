759ddf26b1c4259f2d3f229b3ef481eb01690c0a
thryec <thryec@gmail.com>
2021-11-13 16:47:13 +0800

    split minting and listing into two separated functions/buttons

--- client/pages/create.js
+++ client/pages/create.js
@@ -6,12 +6,14 @@ import { create } from 'ipfs-http-client'
 import { nftaddress, marketplaceaddress } from '../../config'
 import NFT from '../../hardhat/artifacts/contracts/NFT.sol/NFT.json'
 import Market from '../../hardhat/artifacts/contracts/Marketplace.sol/Marketplace.json'
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
 
 const client = create('https://ipfs.infura.io:5001/api/v0')
 
 const Create = () => {
   const [fileUrl, setFileUrl] = useState('')
-  const [tokenId, setTokenId] = useState(0)
+  const [tokenId, setTokenId] = useState(5)
   const [itemInfo, setItemInfo] = useState({
     name: '',
     description: '',
@@ -44,13 +46,13 @@ const Create = () => {
       const addedFile = await client.add(data)
       const url = `https://ipfs.infura.io/ipfs/${addedFile.path}`
       console.log('image successfully uploaded to IPFS at url: ', url)
-      listOnMarketplace(url)
+      mintNFT(url)
     } catch (error) {
       console.log('Error uploading file: ', error)
     }
   }
 
-  const listOnMarketplace = async (url) => {
+  const mintNFT = async (url) => {
     const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
@@ -61,21 +63,34 @@ const Create = () => {
     console.log('starting mint.....')
     const mintTxn = await nftContract.mintToken(myAddress, tokenId, itemInfo.quantity.toString(), '0x00')
     const txn = await mintTxn.wait()
-    console.log('txn: ', txn)
+    console.log('mintTxn: ', txn)
 
-    console.log('setting tokenURI...')
-    // await nftContract.setTokenURI(1, url)
+    await nftContract.setTokenURI(tokenId, url)
+    console.log('tokenURI set')
+    // const img = await nftContract.getTokenURI(3)
+    // console.log('token URI: ', img)
+  }
 
+  const listOnMarketplace = async (url) => {
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
+    const nftContract = new ethers.Contract(nftaddress, NFT.abi, signer)
     const marketplaceContract = new ethers.Contract(marketplaceaddress, Market.abi, signer)
     const price = ethers.utils.parseUnits(itemInfo.price, 'ether')
     console.log('price: ', price)
+
     // approve user to transfer token from NFT contract to Marketplace contract
     await nftContract.connect(signer).setApprovalForAll(marketplaceContract.address, true)
     console.log('signer approved')
+
+    // list on marketplace
     const listTxn = await marketplaceContract.listItemForSale(nftaddress, tokenId, 1, price)
     await listTxn.wait()
-    console.log('listTxn: ', listTxn)
-    console.log('item listed')
+    console.log('tokenId minted: ', tokenId)
+    console.log('listing txn: ', listTxn)
     setFileUrl('')
     setTokenId(tokenId + 1)
   }
@@ -85,10 +100,9 @@ const Create = () => {
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
-    const myAddress = await signer.getAddress()
 
     const nftContract = new ethers.Contract(nftaddress, NFT.abi, signer)
-    const img = await nftContract.getTokenURI(0)
+    const img = await nftContract.getTokenURI(5)
     console.log('token URI: ', img)
   }
 
@@ -96,24 +110,14 @@ const Create = () => {
     <div style={bodyStyle}>
       <h2>Upload Your Creation Here</h2>
       <div style={inputStyle}>
-        <input
-          placeholder="Item Name"
-          onChange={(e) => setItemInfo({ ...itemInfo, name: e.target.value })}
-          style={inputFields}
-          value={itemInfo.name}
-        />
+        <input placeholder="Item Name" onChange={(e) => setItemInfo({ ...itemInfo, name: e.target.value })} style={inputFields} value={itemInfo.name} />
         <textarea
           placeholder="Item Description"
           onChange={(e) => setItemInfo({ ...itemInfo, description: e.target.value })}
           style={inputFields}
           value={itemInfo.description}
         />
-        <input
-          placeholder="List Price"
-          onChange={(e) => setItemInfo({ ...itemInfo, price: e.target.value })}
-          style={inputFields}
-          value={itemInfo.price}
-        />
+        <input placeholder="List Price" onChange={(e) => setItemInfo({ ...itemInfo, price: e.target.value })} style={inputFields} value={itemInfo.price} />
         <input
           placeholder="List Quantity"
           onChange={(e) => setItemInfo({ ...itemInfo, quantity: e.target.value })}
@@ -121,14 +125,26 @@ const Create = () => {
           value={itemInfo.quantity}
         />
         <input type="file" onChange={onFileUpload} style={inputFields} />
-        <button
-          onClick={() => {
-            createSaleItem()
-          }}
-          style={inputFields}
-        >
-          List Item
-        </button>
+        <Stack spacing={2} direction="row">
+          <Button
+            onClick={() => {
+              createSaleItem()
+            }}
+            style={inputFields}
+            variant="contained"
+          >
+            Mint NFT
+          </Button>
+          <Button
+            onClick={() => {
+              listOnMarketplace()
+            }}
+            style={inputFields}
+            variant="contained"
+          >
+            List NFT
+          </Button>
+        </Stack>
         <div>
           <button onClick={fetchTokenURI}>Fetch Token URI</button>
         </div>
@@ -145,6 +161,7 @@ const bodyStyle = {
 const inputStyle = {
   display: 'flexbox',
 }
+
 const inputFields = {
   width: '750px',
   marginTop: 10,
