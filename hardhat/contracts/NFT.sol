0de98c1ed3837c25167ab5f9d4ab1d5df1b39830
thryec <thryec@gmail.com>
2021-11-15 22:52:34 +0800

    added Nat-Spec comments for NFT.sol

--- hardhat/contracts/NFT.sol
+++ hardhat/contracts/NFT.sol
@@ -7,19 +7,31 @@ import '@openzeppelin/contracts/utils/Counters.sol';
 import 'hardhat/console.sol';
 
 contract NFT is ERC1155, Ownable {
+    /// @notice _tokenIds to keep track of the number of NFTs minted
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+
+    /// @notice address of the Marketplace to list the NFTs on. Initialized in the constructor.
+
     address marketplaceAddress;
 
-    // to store unique URIs for each nft
+    /// @notice tokenId => token URIs
     mapping(uint => string) private _uris;
 
-    constructor(address marketAddress) ERC1155('VastOcean') {
+    /// @notice initializes marketplace address to be used for setting approvals to allow marketplace to list the tokens.
+    constructor(address marketAddress) ERC1155() {
         marketplaceAddress = marketAddress;
     }
 
     // ------------------ Mutative Functions ---------------------- //
 
+    /**
+        @notice Mints ERC1155 tokens to the caller's wallet. 
+        @dev Sets approval for the marketplace address to allows listings, and sets the tokenURI in the _uris mapping. 
+        @param tokenURI metadata of the NFT to be minted 
+        @param _quantity number of NFTs to be minted 
+        @return tokenId of the NFT minted 
+    */
     function mintToken(
         string memory tokenURI,
         uint quantity,
@@ -35,6 +47,12 @@ contract NFT is ERC1155, Ownable {
         return _tokenId;
     }
 
+    /**
+        @notice Sets the token URI in the mapping for each NFT when minted.
+        @dev Called by the mintToken function. Limits the URI for each token to 1. 
+        @param tokenId token whose URI is to be set 
+        @param newURI URI to add for this tokenId 
+    */
     function setTokenURI(uint _tokenId, string memory newURI) public {
         bool owner = checkIfOwner(_tokenId);
         require(owner == true);
@@ -42,10 +60,15 @@ contract NFT is ERC1155, Ownable {
         _uris[_tokenId] = newURI;
     }
 
-    function burnTokens(
-        uint _tokenId,
-        uint quantity
-    ) public {
+    /**
+        @notice Allows the owner of the NFT to send it to the zero address 
+        @dev Throws an error if the person calling the burn function is not the owner of the NFT. 
+        @param _tokenId Id of the token to be burned 
+        @param quantity amount of the token to be burned 
+    */
+    function burnTokens(uint _tokenId, uint quantity) public {
+        bool owner = checkIfOwner(_tokenId);
+        require(owner == true);
         _burn(msg.sender, _tokenId, quantity);
     }
 
@@ -59,7 +82,7 @@ contract NFT is ERC1155, Ownable {
         return marketplaceAddress;
     }
 
-    function checkIfOwner( uint _tokenId) public view returns (bool) {
+    function checkIfOwner(uint _tokenId) public view returns (bool) {
         if (balanceOf(msg.sender, _tokenId) > 0) {
             return true;
         }
