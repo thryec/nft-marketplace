5b3150e6b50204233cf71f5b443c7b57f8eb96cb
thryec <thryec@gmail.com>
2021-11-17 11:03:16 +0800

    fetched ipfs image link in gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -6,35 +6,38 @@ import NFT from '../../hardhat/artifacts/contracts/NFT.sol/NFT.json'
 import Market from '../../hardhat/artifacts/contracts/Marketplace.sol/Marketplace.json'
 
 const myGallery = () => {
-  const [myNFTs, setMyNFTs] = useState([])
-  const [isLoaded, setIsLoaded] = useState(false)
+    const [myNFTs, setMyNFTs] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
 
-  const fetchMyNFTs = async () => {
-    const web3modal = new Web3Modal()
-    const connection = await web3modal.connect()
-    const provider = new ethers.providers.Web3Provider(connection)
-    const signer = provider.getSigner()
+    const fetchMyNFTs = async () => {
+        const web3modal = new Web3Modal()
+        const connection = await web3modal.connect()
+        const provider = new ethers.providers.Web3Provider(connection)
+        const signer = provider.getSigner()
 
-    const nftContract = new ethers.Contract(nftaddress, NFT.abi, signer)
-    const marketplaceContract = new ethers.Contract(marketplaceaddress, Market.abi, signer)
+        const nftContract = new ethers.Contract(nftaddress, NFT.abi, signer)
+        const marketplaceContract = new ethers.Contract(marketplaceaddress, Market.abi, signer)
 
-    console.log('fetching items')
-    const data = await marketplaceContract.getItemsOwned()
-    console.log('my items: ', data)
+        console.log('fetching items')
+        const data = await marketplaceContract.getItemsOwned()
+        console.log('my items: ', data)
 
-    data.map((el) => {
-      console.log(el)
-    })
-  }
+        const items = data.map(async (el) => {
+            const tokenURI = await nftContract.getTokenURI(el.tokenId)
+            const res = await fetch(tokenURI)
+            const data = await res.json()
+            console.log(el.itemId, data.image)
+        })
+    }
 
-  useEffect(() => {
-    // fetchMyNFTs()
-  }, [])
+    useEffect(() => {
+        fetchMyNFTs()
+    }, [])
 
-  return isLoaded ? <div style={bodyStyle}>Display NFTs here</div> : <div style={bodyStyle}>No Assets Owned</div>
+    return isLoaded ? <div style={bodyStyle}>Display NFTs here</div> : <div style={bodyStyle}>No Assets Owned</div>
 }
 const bodyStyle = {
-  margin: 40,
+    margin: 40,
 }
 
 export default myGallery
